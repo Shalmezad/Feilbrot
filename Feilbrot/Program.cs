967341895f0b7b3f4241d17916563047d804bfef
Richard Wardin <shalmezad@gmail.com>
2021-01-24 21:28:59 -0500

    Convert row to be processed in parallel, cutting time to 1/4

--- Feilbrot/Program.cs
+++ Feilbrot/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.PixelFormats;
@@ -24,20 +28,28 @@ namespace Feilbrot
                 for(int y = 0; y < image.Height; y++)
                 {
                     Span<Rgba32> pixelRowSpan = image.GetPixelRowSpan(y);
-                    for(int x = 0; x < image.Width; x++)
+
+                    Rgba32[] row = new Rgba32[image.Width];
+                    var source = Enumerable.Range(0, image.Width).ToArray();
+                    Parallel.ForEach(source, (x) =>
                     {
                         // Convert image x/y to window r/i
                         decimal percX = x * 1.0M / image.Width;
                         decimal percY = y * 1.0M / image.Height;
                         ComplexPoint2d testPoint = window.PointAtPercent(percX, percY);
-                        int result = brot.PointInSet(testPoint, 100);
+                        int result = brot.PointInSet(testPoint, 1000);
                         if(result == -1){
-                            pixelRowSpan[x] = new Rgba32(0,0,0,255);
+                            row[x] = new Rgba32(0,0,0,255);
                         }
                         else{
                             float color = result / 100f;
-                            pixelRowSpan[x] = new Rgba32(color, color, 0.5f, 1.0f);
+                            row[x] = new Rgba32(color, color, 0.5f, 1.0f);
                         }
+                    });
+
+                    for(int x = 0; x < image.Width; x++)
+                    {
+                        pixelRowSpan[x] = row[x];
                     }
                 }
                 image.Save("test.png"); 
