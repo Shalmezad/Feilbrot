0fc94d45802ff27918dcc6e91867ad1e2beaf404
Richard Wardin <shalmezad@gmail.com>
2021-01-25 19:40:28 -0500

    Use Partitioner to help speed up processing a pixel row

--- Feilbrot/Program.cs
+++ Feilbrot/Program.cs
@@ -16,11 +16,12 @@ namespace Feilbrot
     {
         static void Main(string[] args)
         {
-            IMandel2d brot = new Circlebrot();
+            IMandel2d brot = new Chickenbrot();
             ComplexRect2d window = brot.PreferredWindow();
 
-            int width = 640;
-            int height = 480;
+            int width = 128;
+            int height = 128;
+            int iterations = 500;
 
 
             using(var image = new Image<Rgba32>(width, height))
@@ -31,19 +32,25 @@ namespace Feilbrot
 
                     Rgba32[] row = new Rgba32[image.Width];
                     var source = Enumerable.Range(0, image.Width).ToArray();
-                    Parallel.ForEach(source, (x) =>
+                    var rangePartitioner = Partitioner.Create(0, source.Length);
+
+                    Parallel.ForEach(rangePartitioner, (range, loopState) =>
                     {
-                        // Convert image x/y to window r/i
-                        decimal percX = x * 1.0M / image.Width;
-                        decimal percY = y * 1.0M / image.Height;
-                        ComplexPoint2d testPoint = window.PointAtPercent(percX, percY);
-                        int result = brot.PointInSet(testPoint, 1000);
-                        if(result == -1){
-                            row[x] = new Rgba32(0,0,0,255);
-                        }
-                        else{
-                            float color = result / 100f;
-                            row[x] = new Rgba32(color, color, 0.5f, 1.0f);
+                        for(int x = range.Item1; x < range.Item2; x++)
+                        {
+                            // Convert image x/y to window r/i
+                            decimal percX = x * 1.0M / image.Width;
+                            decimal percY = y * 1.0M / image.Height;
+                            ComplexPoint2d testPoint = window.PointAtPercent(percX, percY);
+                            int result = brot.PointInSet(testPoint, iterations);
+                            if(result == -1){
+                                row[x] = new Rgba32(0,0,0,255);
+                            }
+                            else{
+                                // TODO: Should probably come up with a better way of making colors pop than multiplying by 10 causing it to hit the ceiling.
+                                float color = result * 10.0f / iterations;
+                                row[x] = new Rgba32(0.1f, color * 0.5f + 0.1f, color * 0.9f + 0.1f, 1.0f);
+                            }
                         }
                     });
 
